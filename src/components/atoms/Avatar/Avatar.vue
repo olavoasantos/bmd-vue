fe54fbf5042814a89f1aa879464d674e5158f470
Olavo <olavo.a.santos@gmail.com>
2018-09-18 12:31:13 -0300

    separated stylesheet from component

--- src/components/atoms/Avatar/Avatar.vue
+++ src/components/atoms/Avatar/Avatar.vue
@@ -25,67 +25,7 @@
 </template>
 
 <style lang="scss">
-  @import "@/components/nucleus/index.scss";
-
-  .avatar {
-    & {
-      display: flex;
-      user-select: none;
-      align-items: center;
-      background-size: cover;
-      justify-content: center;
-      background-position: center
-    }
-
-    &-icon-sm {
-      width: 50%;
-      height: 50%;
-      margin-top: -4px;
-    }
-
-    &-icon-md {
-      width: 40%;
-      height: 40%;
-      margin-top: -8px;
-    }
-
-    &-icon-lg {
-      width: 50%;
-      height: 50%;
-      margin-top: -8px;
-    }
-
-    &-silhouette {
-      width: 100%;
-      height: 100%
-    }
-
-    &-round {
-      border-radius: border-radius(round)
-    }
-
-    &-rounded {
-      border-radius: border-radius(lg)
-    }
-
-    &-sm {
-      width: spacing(xl);
-      height: spacing(xl);
-      font-size: spacing(sm);
-    }
-
-    &-md {
-      width: spacing(3xl);
-      height: spacing(3xl);
-      font-size: spacing(lg);
-    }
-
-    &-lg {
-      width: spacing(5xl);
-      height: spacing(5xl);
-      font-size: spacing(2xl);
-    }
-  }
+  @import "./Avatar.scss";
 </style>
 
 <script>
