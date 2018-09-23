fd8a140d51370420a59bf3b5dd9adc8bde493730
Olavo Amorim Santos <olavoasantos@users.noreply.github.com>
2018-09-23 17:19:00 -0300

    [FEATURE] Card component (#3)
    * added Button component
    
    * designed the Button component
    
    * added Button documentation
    
    * Created, designed and documented the Card component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>
-    <Button
-      label="test"
-    />
+  <div style="padding: 2rem">
+    <Card border="blue-light">
+      <div style="padding: 1rem">
+        test
+      </div>
+    </Card>
+    <br>
+    <Card border="purple-light">
+      <div style="padding: 1rem">
+        test
+      </div>
+    </Card>
+    <br>
+    <Card border="yellow">
+      <div style="padding: 1rem">
+        test
+      </div>
+    </Card>
   </div>
 </template>
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Card } from '@/components/atoms/Card/Card.vue';
 export { default as Badge } from '@/components/atoms/Badge/Badge.vue';
 export { default as Avatar } from '@/components/atoms/Avatar/Avatar.vue';
 export { default as Button } from '@/components/atoms/Button/Button.vue';
